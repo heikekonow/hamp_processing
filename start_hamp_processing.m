cdd6b9def9c062a9e491e5b2526364e208ddb5cd
Heike Konow <heike.konow@uni-hamburg.de>
2020-02-02 18:02:07 -0400

    adjust function calls

--- start_hamp_processing.m
+++ start_hamp_processing.m
@@ -1,9 +1,12 @@
 
 %% Specify time frame for data conversion
 % Start date
-t1 = '20190517';  
+t1 = '20200122';  
 % End date
-t2 = '20190517';
+t2 = '20200202';
+
+% ! Add flight information to file flight_dates.m if they aren't already in
+% there
 
 % Get flight dates to use in this program
 flightdates_use = specifyDatesToUse(t1,t2);
@@ -11,9 +14,9 @@ flightdates_use = specifyDatesToUse(t1,t2);
 %%
 
 
-checkfolderstructure
+checkfolderstructure(getPathPrefix, flightdates_use)
 
 runRadarAttitude(flightdates_use)
 
-run_unifyGrid
+run_unifyGrid(flightdates_use)
 
