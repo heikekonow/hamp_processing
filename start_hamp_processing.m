4eb2d8779a2241592c99c7b3e32ba2097476a4e6
Heike Konow <heike.konow@uni-hamburg.de>
2020-04-28 13:51:26 +0200

    different test case

--- start_hamp_processing.m
+++ start_hamp_processing.m
@@ -7,9 +7,9 @@ contact = 'heike.konow@uni-hamburg.de';
 
 %% Specify time frame for data conversion
 % Start date
-t1 = '20200124';  
+t1 = '20200131';  
 % End date
-t2 = '20200124';
+t2 = '20200131';
 
 % ! Add flight information to file flight_dates.m if they aren't already in
 % there
