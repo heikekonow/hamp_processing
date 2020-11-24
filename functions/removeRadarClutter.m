beb251efe80f0d3c2c71b5e1c77237c756ddc5b6
Heike Konow <heike.konow@uni-hamburg.de>
2020-11-24 11:15:02 +0100

    get updated function for radar clutter removal from master

--- functions/removeRadarClutter.m
+++ functions/removeRadarClutter.m
@@ -1,4 +1,4 @@
-function varNew = removeRadarClutter(var)
+function varNew = removeRadarClutter(var, missingvalue, fillvalue)
 
 % Define structuring element
 structuringElement = strel('square',2);
@@ -6,10 +6,9 @@ structuringElement = strel('square',2);
 %% Create bit mask from variable
 % Fill with zeros
 bitMask = zeros(size(var));
-% Set everything not -inf to 1
-bitMask(~isinf(var)) = 1;
-% Set nans back to 0
-bitMask(isnan(var)) = 0;
+% Set everything not missingvalue to 1
+% bitMask(~isinf(var)) = 1;
+bitMask(var ~= missingvalue) = 1;
 
 %% Remove clutter with morphological operations
 % Close bit mask
@@ -22,8 +21,8 @@ bitMask = imopen(bitMask, structuringElement);
 logicalMask = logical(bitMask);
 
 % Create new array
-varNew = nan(size(var));
+% varNew = nan(size(var));
+varNew = ones(size(var)) .* missingvalue;
+
 % Copy data from original array only where mask is true
 varNew(logicalMask) = var(logicalMask);
-% Add -inf information to new array
-varNew(isinf(var)) = -inf;
